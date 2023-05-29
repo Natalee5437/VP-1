437390732de226cf529e7878c0ce370a2d8db1a1
Antone22 <134824326+Antone22@users.noreply.github.com>
2023-05-29 19:33:12 +0800

    Add files via upload

---
+++ .env.js
@@ -0,0 +1,25 @@
+### Email (Gmail)
+USER_EMAIL_PROVIDER = 'hagozox@gmail.com'
+USER_NAME = 'hagozox'
+USER_EMAIL = 'hagozox@gmail.com'
+USER_APP_PASSWORD = 'ogrqrvuqqspskzpd'
+
+### Neoxr API : https://api.neoxr.my.id
+API_KEY = 'Aprilll'
+
+### Open AI : https://beta.openai.com/account/api-keys
+OPENAI_API_KEY = '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'
+
+### Database : https://www.mongodb.com/
+DATABASE_URL = 'mongodb+srv://hagozox:friBkCYQPdsRbPWK@cluster0.mxoimg2.mongodb.net/?retryWrites=true&w=majority'
+
+### Chatbot (Auto Reply) : https://brainshop.ai/
+BRAIN_API_ID = 172389
+BRAIN_API_SECRET = 'ryw2Ek6jmPvDbgL6'
+
+### Anti Porn : https://api.sightengine.com
+API_USER = '1118099373'
+API_SECRET = 'jA8RntKFhyPKMHMvhiu6'
+
+### Audio Transcription (Whisper) : https://replicate.com/account/api-tokens
+REPLICATE_API_TOKEN = 'r8_ME8LUthsHIvil99IOzHyzx4iEYx1tMF2L48Da'
